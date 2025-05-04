16246b096ddfe5d62e532c8f2e23187b71b6da9e
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-05-04 11:15:20 +0600

    for and if else

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fruits := []string{"apple", "banana", "orange"}
+	const (
+		core = 500
+		host = "Localhost"
+	)
+	var (
+		isNameAvailable = false
+		isString        = "yes"
+	)
+	fruits := deck{"apple", "banana", "orange"}
 	fruits = append(fruits, "grape", "pineapple")
-	// var moreFruits []string = []string{"kiwi", "strawberry"}
-	for in, fruit := range fruits {
-		fmt.Printf("%d %s\n", in+1, fruit)
-	}
 
-	for i := range fruits {
-		fmt.Println(fruits[i])
+	hand, remainingCrads := deal(fruits, 3)
+	fmt.Println(hand, remainingCrads)
+	fmt.Println(isNameAvailable, isString)
+	fmt.Println(core, host)
+	forAsWhile()
+	classicForLoop()
+}
+
+func forAsWhile() {
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i += 1
+	}
+}
+
+func classicForLoop() {
+	var (
+		even   []int
+		divBy3 []int
+	)
+
+	for j := range 10 {
+		if j%2 == 0 {
+			even = append(even, j)
+		} else if j%3 == 0 {
+			divBy3 = append(divBy3, j)
+		} else {
+			fmt.Println("Print nothing")
+		}
 	}
 }
