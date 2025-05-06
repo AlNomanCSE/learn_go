1eb93fc36836c3b948e2ba142f3b2056d0d1e09a
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-05-06 22:57:19 +0600

    ponuer ,closures , struct(class by known)

---
+++ struct/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type customer struct {
+	name   string
+	mobile string
+}
+type order struct {
+	id        string
+	ammount   float32
+	status    string
+	createdAt time.Time
+	customer
+}
+
+func newOrder(id string, ammount float32, status string, cust customer) *order {
+	myOrder := order{
+		id:        id,
+		ammount:   ammount,
+		status:    status,
+		createdAt: time.Now(),
+		customer:  cust,
+	}
+	return &myOrder
+}
+
+func (o *order) updateAmount(newAmout float32) {
+	o.ammount = newAmout
+}
+func main() {
+	newcustomer := customer{
+		name:   "Noman",
+		mobile: "01883217001",
+	}
+	myoder := newOrder("order-1", 12.33, "pending", newcustomer)
+	// myoder.customer = newcustomer
+	fmt.Println(myoder)
+	myoder.updateAmount(1234.45)
+	fmt.Println(myoder)
+
+}
