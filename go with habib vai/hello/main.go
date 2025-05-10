11313067f8c3e8ccde64f8a1908b6d2f7e394459
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-05-10 11:23:26 +0600

    if else

---
+++ go with habib vai/hello/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 18
+	if age > 18 {
+		fmt.Println("Eligibale to merride")
+	} else if age < 18 {
+		fmt.Println("Not Eligibale to merride")
+	} else {
+		fmt.Println("Porashuno koro vai")
+	}
+
+}
