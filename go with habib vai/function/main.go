93d3f5ae4de92ef8f6ebb046f7a8efa4c25c5346
AlNomanCSE <abdullahalnomancse@gmail.com>
2025-05-11 14:08:30 +0600

    add func

--- go with habib vai/function/main.go
+++ go with habib vai/function/main.go
@@ -1,17 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type greeter struct{}
 
 func (g greeter) sayHello() string {
 	return "Hello from a method!"
 }
-func (g greeter) sayHello() int {
-	return 123
-}
+
 func main() {
 	g := greeter{}
 	fmt.Println(g.sayHello())
-
+	// mathlib.Add(12, 13)
+	add()
 }
---
+++ go with habib vai/function/mathlib/add.go
@@ -0,0 +1,7 @@
+package mathlib
+
+import "fmt"
+
+func Add(a int, b int) {
+	fmt.Println(a + b)
+}
